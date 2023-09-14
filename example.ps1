13833664b3a7a72e0f22b10f608a6777b1e5c28c
lvalovits <lvalovits@gmail.com>
2023-09-14 19:32:03 -0300

    Session object tested

--- example.ps1
+++ example.ps1
@@ -20,105 +20,105 @@ Import-Module $PSScriptRoot\isvg_im_lib\utils\isvg_im_lib_init.psm1 -force
 # . ".\isvg_im_lib\proxies\Proxy_OrganizationalUnit.ps1"
 
 _init_ #-SkipTest_Connections
-exit
-# function Test-Connection(){
-# 	$session_proxy	=	[Session_Proxy]::getProxy()
-# 	$session_proxy.init()
+
+function Test-Connection(){
+	$session_proxy	=	[Session_Proxy]::getProxy()
+	$session_proxy.init()
 	
-# 	$creds			=	$null
+	$creds			=	$null
 
-# 	try{
-# 		$creds		=	Get-Credential -Credential $null
-# 	}catch{
-# 		Write-Host -fore red "$($Error[0])"
-# 	}
+	try{
+		$creds		=	Get-Credential -Credential $null
+	}catch{
+		Write-Host -fore red "$($Error[0])"
+	}
 
-# 	if ( $null -ne $creds){
-# 		$session_proxy.login($creds)
+	if ( $null -ne $creds){
+		$session_proxy.login($creds)
 	
-# 		if ( -not ( [ISIM_Session]::GetSession().isEmpty() ) ){
-# 			Write-Host "Login success for user" $creds.UserName
-# 			Write-Host -ForegroundColor green 'Session object is a singleton. You can access it through [ISIM_Session]::GetSession()'
-# 		}
-# 	}
+		if ( -not ( [ISIM_Session]::GetSession().isEmpty() ) ){
+			Write-Host "Login success for user" $creds.UserName
+			Write-Host -ForegroundColor green 'Session object is a singleton. You can access it through [ISIM_Session]::GetSession()'
+		}
+	}
 
-# 	Write-Host
-# }
+	Write-Host
+}
 
-# function Test-SearchRoles(){
-# 	$isim_session 		=	[ISIM_Session]::GetSession()
+function Test-SearchRoles(){
+	$isim_session 		=	[ISIM_Session]::GetSession()
 
-# 	$role_proxy			=	[ISIM_Role_Proxy]::getProxy()
-# 	$role_proxy.init()
-# 	$isim_roles 		= $role_proxy.searchRoles($isim_session.raw , "(errolename=*)")
+	$role_proxy			=	[ISIM_Role_Proxy]::getProxy()
+	$role_proxy.init()
+	$isim_roles 		= $role_proxy.searchRoles($isim_session.raw , "(errolename=*)")
 
-# 	Write-Host "Roles count:		" $isim_roles.count
-# 	Write-Host "	Static roles:	" $($isim_roles | Where-Object {$_.membership_type() -eq 1}).count
-# 	Write-Host "	Dynamic roles:	" $($isim_roles | Where-Object {$_.membership_type() -eq 2}).count
+	Write-Host "Roles count:		" $isim_roles.count
+	Write-Host "	Static roles:	" $($isim_roles | Where-Object {$_.membership_type() -eq 1}).count
+	Write-Host "	Dynamic roles:	" $($isim_roles | Where-Object {$_.membership_type() -eq 2}).count
 	
-# 	# Global variable to export organizational structure search result for demo purposes
-# 	$Global:isim_roles 	= $isim_roles
-# 	Write-Host -ForegroundColor green 'Roles stored on $Global:isim_roles variable.'
-# 	Write-Host
-# }
+	# Global variable to export organizational structure search result for demo purposes
+	$Global:isim_roles 	= $isim_roles
+	Write-Host -ForegroundColor green 'Roles stored on $Global:isim_roles variable.'
+	Write-Host
+}
 
-# function Test-SearchOrganizationalStructure(){
-# 	$isim_session 				=	[ISIM_Session]::GetSession()
-# 	$ou_proxy					=	[ISIM_OrgUnit_Proxy]::getProxy()
-# 	$ou_proxy.init()
+function Test-SearchOrganizationalStructure(){
+	$isim_session 				=	[ISIM_Session]::GetSession()
+	$ou_proxy					=	[ISIM_OrgUnit_Proxy]::getProxy()
+	$ou_proxy.init()
 
-# 	$isim_organization			=	$ou_proxy.getOrganization( $isim_session.raw, $GLOBAL:ISIM_WS_Props['ORGANIZATION_NAME'] )
+	$isim_organization			=	$ou_proxy.getOrganization( $isim_session.raw, $GLOBAL:ISIM_WS_Props['ORGANIZATION_NAME'] )
 	
-# 	$isim_organization_acmeInc	=	$isim_organization | Where-Object { $_.name -eq "Acme Inc."}
+	$isim_organization_acmeInc	=	$isim_organization | Where-Object { $_.name -eq "Acme Inc."}
 
-# 	$isim_subtree				=	$ou_proxy.getOrganizationSubTree( $isim_session.raw , $isim_organization_acmeInc.raw)
+	$isim_subtree				=	$ou_proxy.getOrganizationSubTree( $isim_session.raw , $isim_organization_acmeInc.raw)
 
-# 	Write-Host "Organizations count:	" $isim_organization.count	"'#TODO: remove filter by org name?'"
-# 	Write-Host "Subtree count:		" $isim_subtree.count			"'#TODO: count child items'"
+	Write-Host "Organizations count:	" $isim_organization.count	"'#TODO: remove filter by org name?'"
+	Write-Host "Subtree count:		" $isim_subtree.count			"'#TODO: count child items'"
 
-# 	# Global variable to export organizational structure search result for demo purposes
-# 	$Global:isim_organization	=	$isim_organization
-# 	$Global:isim_subtree 		=	$isim_subtree
-# 	Write-Host -ForegroundColor green 'Organizations have been stored on $Global:isim_organization variable.'
-# 	Write-Host -ForegroundColor green 'Organizational Structure have been stored on $Global:isim_subtree variable.'
-# }
+	# Global variable to export organizational structure search result for demo purposes
+	$Global:isim_organization	=	$isim_organization
+	$Global:isim_subtree 		=	$isim_subtree
+	Write-Host -ForegroundColor green 'Organizations have been stored on $Global:isim_organization variable.'
+	Write-Host -ForegroundColor green 'Organizational Structure have been stored on $Global:isim_subtree variable.'
+}
 
-# function Test-CreateStaticRoles(){
-# 	$isim_session 						=	[ISIM_Session]::GetSession()
+function Test-CreateStaticRoles(){
+	$isim_session 						=	[ISIM_Session]::GetSession()
 	
-# 	$role_proxy							=	[ISIM_Role_Proxy]::getProxy()
-# 	$ou_proxy							=	[ISIM_OrgUnit_Proxy]::getProxy()
+	$role_proxy							=	[ISIM_Role_Proxy]::getProxy()
+	$ou_proxy							=	[ISIM_OrgUnit_Proxy]::getProxy()
 
-# 	$isim_organization					=	$ou_proxy.getOrganization( $isim_session.raw )
-# 	$isim_subtree						=	$ou_proxy.getOrganizationSubTree( $isim_session.raw , $isim_organization.raw)
+	$isim_organization					=	$ou_proxy.getOrganization( $isim_session.raw )
+	$isim_subtree						=	$ou_proxy.getOrganizationSubTree( $isim_session.raw , $isim_organization.raw)
 
-# 	$new_role_name						=	"WS v2 Static Test - " + $(timeStamp)
-# 	$new_role_desc						=	"Description for WS v2 Static Test - Next role in 5 seconds"
-# 	$isim_new_staticRole				=	[ISIM_Role]::New($new_role_name, $new_role_desc)
+	$new_role_name						=	"WS v2 Static Test - " + $(timeStamp)
+	$new_role_desc						=	"Description for WS v2 Static Test - Next role in 5 seconds"
+	$isim_new_staticRole				=	[ISIM_Role]::New($new_role_name, $new_role_desc)
 	
-# 	$ws_new_staticRole					=	$role_proxy.getStub()
-# 	Convert-2WSObject $isim_new_staticRole $ws_new_staticRole
+	$ws_new_staticRole					=	$role_proxy.getStub()
+	Convert-2WSObject $isim_new_staticRole $ws_new_staticRole
 
-# 	$isim_role	=	$role_proxy.createStaticRole($isim_session.raw, $isim_subtree.children[0].raw, $new_staticRole_ws)
-# 	Write-Host "Static role created:	" $isim_role.name
+	$isim_role	=	$role_proxy.createStaticRole($isim_session.raw, $isim_subtree.children[0].raw, $new_staticRole_ws)
+	Write-Host "Static role created:	" $isim_role.name
 
-# 	Start-Sleep -Seconds 5
+	Start-Sleep -Seconds 5
 
-# 	$isim_role.name						=	"WS v2 Static Test - " + $(timeStamp)
-# 	$isim_role.attributes.description	=	"Description for WS v2 Static Test"
+	$isim_role.name						=	"WS v2 Static Test - " + $(timeStamp)
+	$isim_role.attributes.description	=	"Description for WS v2 Static Test"
 	
-# 	$isim_role.attributes.erglobalid	= $null
-# 	$isim_role.attributes.erparent		= $null
-# 	$isim_role.attributes.objectclass	= $null
+	$isim_role.attributes.erglobalid	= $null
+	$isim_role.attributes.erparent		= $null
+	$isim_role.attributes.objectclass	= $null
 	
-# 	$ws_new_staticRole_2				=	$role_proxy.getStub()
-# 	Convert-2WSObject $isim_role $ws_new_staticRole_2
+	$ws_new_staticRole_2				=	$role_proxy.getStub()
+	Convert-2WSObject $isim_role $ws_new_staticRole_2
 
-# 	$isim_role	=	$role_proxy.createStaticRole($isim_session.raw, $isim_subtree.children[0].raw, $ws_new_staticRole_2)
-# 	Write-Host "Static role created:	" $isim_role.name
-# }
+	$isim_role	=	$role_proxy.createStaticRole($isim_session.raw, $isim_subtree.children[0].raw, $ws_new_staticRole_2)
+	Write-Host "Static role created:	" $isim_role.name
+}
 
-# Test-Connection
+Test-Connection
 # Test-SearchRoles
 # Test-SearchOrganizationalStructure
 
