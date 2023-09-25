57266e525b461e19d729e08850cda84e463dfaac
lvalovits <lvalovits@duck.com>
2023-09-25 18:23:53 -0300

    Update proxy_session.psm1

--- isvg_im_lib/proxies/proxy_session.psm1
+++ isvg_im_lib/proxies/proxy_session.psm1
@@ -19,13 +19,15 @@ class IM_Session_Proxy{
     # static [IM_Session_Proxy] getProxy() { return [IM_Session_Proxy]::_instance }
 	################# Singleton end #################
 
-	static $version 						=	0.2.0
-	hidden static $subject 					=	"im_session_proxy"
-	static $proxies							=	@()
+	static $version 		=	0.2.0
+	hidden static $subject 	=	"im_session_proxy"
+	static $proxies			=	@()
 
-	$proxy			=	$null
-	$namespace		=	$null
-	$proxy_wsdl		=	$null
+	$proxy					=	$null
+	$namespace				=	$null
+	$proxy_wsdl				=	$null
+
+	[IM_Session] $session	=	$null
 
 	IM_Session_Proxy([IM_Endpoint] $endpoint){
 		try{
@@ -34,6 +36,7 @@ class IM_Session_Proxy{
 			$this.namespace			=	$this.proxy.GetType().Namespace
 
 			[IM_Session_Proxy]::proxies	+=	$this
+			[utils_logs]::write_log("TRACE", "$([IM_Session_Proxy]::subject):	++	New session proxy created: $($this.proxy_wsdl)")
 		}catch{
 			Write-Warning "$([IM_Session_Proxy]::subject): $($PSItem)"
 			[utils_logs]::write_log("error", "$([IM_Session_Proxy]::subject):	++	Exception:	$($PSItem)")
@@ -46,50 +49,47 @@ class IM_Session_Proxy{
 	# default constructor cannot be disable.
 	hidden IM_Session_Proxy() { throw 'Default constructor disabled. To instance a new proxy use IM_Session_Proxy::new( [IM_Endpoint] $endpoint, [IM_Session] $session )' }
 
-	[void] login (){
-		# $im_cred	=	Get-Credential -Credential $null -Message "Enter your ISVG IM credential"
-		$this.login($(Get-Credential -Credential $null -Message "Enter your ISVG IM credential"))
+	[IM_Session] login (){
+		return $($this.login($(Get-Credential -Message "Enter your ISVG IM credential")))
 	}
 
-	[void] login ( [PSCredential] $IM_Credential ){
+	[IM_Session] login ( [PSCredential] $IM_Credential ){
 
 		try{
+			[utils_logs]::write_log("INFO", "$([IM_Session_Proxy]::subject):	++	Loggin in as: $($IM_Credential.GetNetworkCredential().username)")
+
 			$wsReturn								=	$this.proxy.login( $IM_Credential.GetNetworkCredential().username, $IM_Credential.GetNetworkCredential().password )
 			Clear-Variable -Name IM_Credential
 
-			$Session								=	[IM_Session]::new()
+			$this.session							=	[IM_Session]::new()
 
-			$Session.raw							=	$wsReturn
-			$Session.sessionID						=	$wsReturn.sessionID
-			$Session.clientSession					=	$wsReturn.clientSession
-			$Session.enforceChallengeResponse		=	$wsReturn.enforceChallengeResponse
+			$this.session.raw						=	$wsReturn
+			$this.session.sessionID					=	$wsReturn.sessionID
+			$this.session.clientSession				=	$wsReturn.clientSession
+			$this.session.enforceChallengeResponse	=	$wsReturn.enforceChallengeResponse
 			$wsReturn.locale | ForEach-Object{
-				$Session.locale.$($_.name)			=	$_.values
+				$this.session.locale.$($_.name)		=	$_.values
 			}
+
+			[utils_logs]::write_log("INFO", "$([IM_Session_Proxy]::subject):	++	Login success")
+			
+			return $($this.session)
 		}catch{
 			Write-Warning "$([IM_Session_Proxy]::subject): $($PSItem)"
 			[utils_logs]::write_log("error", "$([IM_Session_Proxy]::subject):	++	Exception:	$($PSItem)")
 			[utils_logs]::write_log("debug", "$([IM_Session_Proxy]::subject):	++	Ex.Message:	$($PSItem.exception.Message)")
 			[utils_logs]::write_log("debug", "$([IM_Session_Proxy]::subject):	++	$($PSItem.InvocationInfo.Scriptname.toString().split('\')[-1]):$($PSItem.InvocationInfo.ScriptLineNumber).")
+			
+			return $null
 		}
 	}
 
-	[void] logout ( $raw_session ){
+	[void] logout (){
 		try{
-			$this.proxy.logout( $raw_session )
-		
-			$Session							=	[IM_Session]::GetSession()
-			$Session.raw						=	$null
-			$Session.sessionID					=	$null
-			$Session.clientSession				=	$null
-			$Session.locale.country				=	$null
-			$Session.locale.variant				=	$null
-			$Session.locale.language			=	$null
-			$Session.enforceChallengeResponse	=	$null
+			$this.proxy.logout( $this.session.raw )
+			$this.session	=	$null
 
-			Write-Host
-			Write-Host "Successfully logout" -ForegroundColor green
-			Write-Host
+			[utils_logs]::write_log("INFO", "$([IM_Session_Proxy]::subject):	++	Logout success")
 
 		}catch{
 			Write-Warning "$([IM_Session_Proxy]::subject): $($PSItem)"
